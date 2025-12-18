7ebd041b5896849eceee70588dc82a865c2bf9b1
bala-amypo <bala@amypo.in>
2025-12-18 16:17:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserPortfolioController.java
+++ src/main/java/com/example/demo/controller/UserPortfolioController.java
@@ -2,6 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.UserPortfolio;
+import com.example.demo.service.UserPortfolioService;
 
 @RestController
 public class UserPortfolioController{
