ac148f1512ba3e79ef81d5bd45bf5abc08b4837d
bala-amypo <bala@amypo.in>
2025-12-18 16:32:46 +0000

    User commit

--- src/main/java/com/example/demo/service/UserPortfolioService.java
+++ src/main/java/com/example/demo/service/UserPortfolioService.java
@@ -2,8 +2,11 @@ package com.example.demo.service;
 import com.example.demo.model.UserPortfolio;
 public interface UserPortfolioService{
     UserPortfolio createPortfolio(UserPortfolio portfolio);
+    UserPortfolio updatePortfolio(Long id,UserPortfolio portfolio );
+    UserPortfolio getPortfolioById(Long id);
+    UserPortfolio getPortfoliosById(Long id);
     String DeleteData(int id);
-    StudentEntity getData(int id);
-    StudentEntity updateData(int id,StudentEntity entity);
+    
+    
 
 }
