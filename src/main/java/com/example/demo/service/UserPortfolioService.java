5c1e4fdd83842dfad7042713407a94ac9c203dd2
bala-amypo <bala@amypo.in>
2025-12-18 15:56:47 +0000

    User commit

--- src/main/java/com/example/demo/service/UserPortfolioService.java
+++ src/main/java/com/example/demo/service/UserPortfolioService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 import com.example.demo.model.UserPortfolio;
 public interface UserPortfolioService{
+    UserPortfolio createPortfolio(UserPortfolio portfolio);
     
 }
