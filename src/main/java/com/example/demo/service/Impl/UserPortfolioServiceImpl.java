1c3b9e4fdc2acd022462f49a3c2171e036286ba0
bala-amypo <bala@amypo.in>
2025-12-18 15:59:46 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserPortfolioServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserPortfolioServiceImpl.java
@@ -1,4 +1,12 @@
-// package com.example.demo.service.Impl;
-// public class UserPortfolioServiceImpl implements UserPortfolioService{
+package com.example.demo.service.Impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.UserPortfolio;
+import com.example.demo.service.UserPortfolioService;
+import com.example.demo.repository.UserPortfolioRepository;
+
+@Service
+public class UserPortfolioServiceImpl implements UserPortfolioService{
     
-// }
+}
