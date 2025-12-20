1c05b0d4790291b676dbdeab31f9ae5bd8429ca5
bala-amypo <bala@amypo.in>
2025-12-20 11:25:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/StockRepository.java
+++ src/main/java/com/example/demo/repository/StockRepository.java
@@ -1,4 +1,9 @@
-// package com.example.demo.repository;
-// public interface StockRepository{
-    
-// }
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.Stock;
+@Repository
+public interface StockRepository extends JpaRepository<Stock,Long>{
+
+}
