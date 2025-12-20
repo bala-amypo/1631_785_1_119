4177153402a359bd87720414872923be877c9752
bala-amypo <bala@amypo.in>
2025-12-20 07:37:18 +0000

    User commit

--- src/main/java/com/example/demo/model/UserPortfolio.java
+++ src/main/java/com/example/demo/model/UserPortfolio.java
@@ -6,8 +6,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class UserPortfolio{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -19,51 +27,6 @@ public class UserPortfolio{
     private Timestamp createdAt;
     private Timestamp updatedAt;
     private boolean action;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Long getUserId() {
-        return userId;
-    }
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-    public String getPortfolioName() {
-        return portfolioName;
-    }
-    public void setPortfolioName(String portfolioName) {
-        this.portfolioName = portfolioName;
-    }
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-    public Timestamp getUpdatedAt() {
-        return updatedAt;
-    }
-    public void setUpdatedAt(Timestamp updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-    public boolean isAction() {
-        return action;
-    }
-    public void setAction(boolean action) {
-        this.action = action;
-    }
-    public UserPortfolio(Long id, Long userId,@NotBlank String portfolioName, Timestamp createdAt, Timestamp updatedAt,
-            boolean action) {
-        this.id = id;
-        this.userId = userId;
-        this.portfolioName = portfolioName;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.action = action;
-    }
-    public UserPortfolio() {
-    } 
+     
+
 }
