bbc7058fd70025563ffc6ec5a7c2af3423fd1bc3
bala-amypo <bala@amypo.in>
2025-12-20 11:39:24 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StockServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StockServiceImpl.java
@@ -14,17 +14,17 @@ public class StockServiceImpl implements StockService{
         return stocks.save(stock);
     }
     @Override
-   public UserPortfolio updatePortfolio(Long id,UserPortfolio portfolio){
-        if(port_folio.existsById(id)){
-            portfolio.setId(id);
-            return port_folio.save(portfolio);
+   public Stock updateStock(Long id,Stock stock){
+        if(stocks.existsById(id)){
+            stock.setId(id);
+            return stocks.save(stock);
         }
         return null;
    }
     
    @Override
-   public UserPortfolio getPortfolioById(Long id){
-        return port_folio.findById(id).orElse(null);
+   public Stock getStockById(Long id){
+        return stocks.findById(id).orElse(null);
 
    }
     @Override
