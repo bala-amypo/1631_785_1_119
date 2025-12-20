22a7d0de95d4afadab2faa7589300343482879a4
bala-amypo <bala@amypo.in>
2025-12-20 12:51:24 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskThresholdService.java
+++ src/main/java/com/example/demo/service/RiskThresholdService.java
@@ -1,4 +1,11 @@
-// package com.example.demo.service;
-// public interface RiskThresholdService{
+
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.model.RiskThreshold;
+public interface StockService{
+    RiskThreshold createStock(RiskThreshold threshold);
+    RiskThreshold updateStock(Long id,RiskThreshold threshold);
+    RiskThreshold getStockById(Long id);
+    List<RiskThreshold>getAllStocks();
     
-// }
+}
