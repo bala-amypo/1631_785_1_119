003de875dbba071dc1aa7cb141ecc64694b99541
bala-amypo <bala@amypo.in>
2025-12-20 11:48:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -1,40 +1,40 @@
-// package com.example.demo.controller;
-// import java.util.List;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.RestController;
-// import com.example.demo.model.Stock;
-// import com.example.demo.service.StockService;
-// import org.springframework.web.bind.annotation.PathVariable;
+package com.example.demo.controller;
+import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.Stock;
+import com.example.demo.service.StockService;
+import org.springframework.web.bind.annotation.PathVariable;
 
-// @RestController
-// public class StockController
-// {
-//     @Autowired StockService ser;
-//     @PostMapping("/Post_Stock")
-//     public Stock create_Portfolio(@RequestBody Stock stock){
-//         return ser.createStock(stock);
-//     }
-//     @PutMapping("/Put_stock/{id}")
-//     public Stock update_Portfolio(@PathVariable Long id,@RequestBody Stock model){
-//         return ser.updateStock(id,model);
-//     }
-//     @GetMapping("/getid1/{id}")
-//     public UserPortfolio get_PortfolioById(@PathVariable Long id){
-//         return ser.getPortfolioById(id);
-//     }
-//     @GetMapping("/get1/user/{userId}")
-//     public List<UserPortfolio> get_PortfoliosByUser(){
-//         return ser.getPortfoliosByUser();
-//     }
-//     @DeleteMapping("/delete1/{id}")
-//     public String deactivate_Portfolio(@PathVariable Long id){
-//         return ser.deactivatePortfolio(id);
+@RestController
+public class StockController
+{
+    @Autowired StockService ser;
+    @PostMapping("/Post_Stock")
+    public Stock create_Stock(@RequestBody Stock stock){
+        return ser.createStock(stock);
+    }
+    @PutMapping("/Put_stock/{id}")
+    public Stock update_Stock(@PathVariable Long id,@RequestBody Stock model){
+        return ser.updateStock(id,model);
+    }
+    @GetMapping("/get_Stock/{id}")
+    public Stock get_StockById(@PathVariable Long id){
+        return ser.getStockById(id);
+    }
+    @GetMapping("/getall_Stock/user/{userId}")
+    public List<Stock> get_AllStocks(){
+        return ser.getAllStocks();
+    }
+    @DeleteMapping("/deleteStock/{id}")
+    public String deactivate_Stock(@PathVariable Long id){
+        return ser.deactivateStock(id);
 
-//     }
+    }
 
-// }
+}
