f66701085fecf2077ca10085cbb2b8b7dea478f7
bala-amypo <bala@amypo.in>
2025-12-20 11:27:24 +0000

    User commit

--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -1,4 +1,5 @@
-// package com.example.demo.service;
-// public interface StockService{
-    
-// }
+package com.example.demo.service;
+import com.example.demo.model.Stock;
+public interface StockService{
+    Stock create
+}
