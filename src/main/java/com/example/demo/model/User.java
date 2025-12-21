de96f11e223f39952a60bd64303579c6e6230aa2
bala-amypo <bala@amypo.in>
2025-12-21 18:27:39 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,19 +1,22 @@
-
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Email;
 import java.time.LocalDateTime;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "users")
+
 public class User {
     
     @Id
@@ -25,7 +28,7 @@ public class User {
     
     @NotNull
     @Email
-    @Column(unique = true)
+    @Column(name="email",unique = true)
     private String email;
     
     @NotNull
@@ -40,7 +43,7 @@ public class User {
         if (this.createdAt == null) {
             this.createdAt = LocalDateTime.now();
         }
-        // Set default role to MONITOR if not provided
+        
         if (this.role == null || this.role.isEmpty()) {
             this.role = "MONITOR";
         }
