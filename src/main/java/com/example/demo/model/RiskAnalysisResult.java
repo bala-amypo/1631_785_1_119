84f09f11b7135a0f79c7b7b1c190710a39ffbba1
bala-amypo <bala@amypo.in>
2025-12-21 15:13:28 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskAnalysisResult.java
+++ src/main/java/com/example/demo/model/RiskAnalysisResult.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotNull;
-// import java.sql.Timestamp;
 import jakarta.persistence.JoinColumn;
 import java.time.LocalDateTime;
 import jakarta.persistence.ManyToOne;
