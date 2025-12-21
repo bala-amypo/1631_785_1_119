91b091ee614684b8d9b298a0a67240de4557aa76
bala-amypo <bala@amypo.in>
2025-12-21 06:15:47 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskAnalysisResultService.java
+++ src/main/java/com/example/demo/service/RiskAnalysisResultService.java
@@ -2,7 +2,8 @@
 package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.RiskAnalysisResult;
-
-// public interface RiskAnalysisResultService{
-    
+public interface RiskAnalysisResultService{
+    RiskAnalysisResult analyzePortfolio(Long portfolioId);
+    RiskAnalysisResult getAnalysisById(Long id);
+    RiskAnalysisResult getAnalysesForPortfolio(Long portfolioId);
 }
