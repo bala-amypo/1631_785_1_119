6c2db4d0f3b5f0c8d322a762736e0eb378edff7f
bala-amypo <bala@amypo.in>
2025-12-21 16:34:28 +0000

    User commit

--- src/main/java/com/example/demo/controller/RiskAnalysisResultController.java
+++ src/main/java/com/example/demo/controller/RiskAnalysisResultController.java
@@ -14,19 +14,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class RiskAnalysisResultController
 {
-    @Autowired UserPortfolioService ser;
-    @PostMapping("/Post_RiskAnalysisResult")
-    public UserPortfolio create_Portfolio(@RequestBody UserPortfolio portfolio){
-        return ser.createPortfolio(portfolio);
+    private final RiskAnalysisResultService ser;
+    public RiskAnalysisResultController(RiskAnalysisResultService ser){
+        this.ser=ser;
     }
+    @PostMapping("/Post_Analysis/analyze/{}")
+    // public RiskAnalysisResult analyzePortfolio(Long portfolioId){
+    //     return ser.createPortfolio(portfolio);
+    // }
     
-    @GetMapping("/getid_RiskAnalysisResult/{id}")
-    public UserPortfolio get_PortfolioById(@PathVariable Long id){
-        return ser.getPortfolioById(id);
-    }
-    @GetMapping("/getall_RiskAnalysisResult/user/{userId}")
-    public List<UserPortfolio> get_PortfoliosByUser(){
-        return ser.getPortfoliosByUser();
-    }
+    // @GetMapping("/getid_RiskAnalysisResult/{id}")
+    // public UserPortfolio get_PortfolioById(@PathVariable Long id){
+    //     return ser.getPortfolioById(id);
+    // }
+    // @GetMapping("/getall_RiskAnalysisResult/user/{userId}")
+    // public List<UserPortfolio> get_PortfoliosByUser(){
+    //     return ser.getPortfoliosByUser();
+    // }
 
 }
