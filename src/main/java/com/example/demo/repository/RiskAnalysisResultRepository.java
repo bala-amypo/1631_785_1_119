eb3794005fc5b798e58264bd27246d706150a873
bala-amypo <bala@amypo.in>
2025-12-21 06:10:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskAnalysisResultRepository.java
+++ src/main/java/com/example/demo/repository/RiskAnalysisResultRepository.java
@@ -1,6 +1,11 @@
-// package com.example.demo.repository;
-// public interface RiskAnalysisResultRepository{
-    
-// }
 
-import jakarta.persistence.JoinColumn;
+
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.RiskAnalysisResult;
+@Repository
+public interface RiskAnalysisResultRepository extends JpaRepository<RiskAnalysisResult,Long>{
+
+}
