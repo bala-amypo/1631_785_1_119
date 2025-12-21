037d274b84d3ae7732c754af857120d762720c0d
bala-amypo <bala@amypo.in>
2025-12-21 15:00:17 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
@@ -11,15 +11,15 @@ import com.example.demo.exception.ResourceNotFoundException;
 @Service
 public class RiskAnalysisResultServiceImpl implements RiskAnalysisResultService{
     private final RiskAnalysisResultRepository Results;
-    private final UserPortfolioRepository portfolio_repo;
-    public RiskAnalysisResultServiceImpl(RiskAnalysisResultRepository Results,UserPortfolioRepository portfolio_repo){
-     this.RiskAnalysisResultRepository=Results;
-     this.UserPortfolioRepository=portfolio_rep;
+//     private final UserPortfolioRepository portfolio_repo;
+    public RiskAnalysisResultServiceImpl(RiskAnalysisResultRepository Results){
+     this.Results=Results;
+     // this.UserPortfolioRepository=portfolio_rep;
     }
     
     @Override
     public RiskAnalysisResult analyzePortfolio(Long portfolioId){
-          portfolio_repo.findById(portfolioId).orElseThrow(()->new ResourceNotFoundException("Result Not found"));
+          // portfolio_repo.findById(portfolioId).orElseThrow(()->new ResourceNotFoundException("Result Not found"));
          return Results.save(portfolioId);
      }
        
