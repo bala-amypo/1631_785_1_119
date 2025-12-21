ea5029068b6b25c4c027e8a8077b7190b14f951a
bala-amypo <bala@amypo.in>
2025-12-21 19:06:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -9,33 +9,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 package com.example.demo.controller;
-
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
     
     private final UserService userService;
+    
     public AuthController(UserService userService) {
         this.userService = userService;
     }
+    
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
+    public User registerUser(@RequestBody User user) {
         return userService.registerUser(user);
     }
-
+    
     @PostMapping("/login")
-    public User login(@RequestBody User loginRequest) {
-        User user = userService.findByEmail(loginRequest.getEmail());
-        if (!user.getPassword().equals(loginRequest.getPassword())) {
+    public User loginUser(@RequestBody User user) {
+        User foundUser = userService.findByEmail(user.getEmail());
+        
+        if(!foundUser.getPassword().equals(user.getPassword())) {
             throw new IllegalArgumentException("Invalid credentials");
         }
-        return user;
-    }
-    @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userService.findById(id);
-    }
-    @GetMapping("/user/email/{email}")
-    public User getUserByEmail(@PathVariable String email) {
-        return userService.findByEmail(email);
+        
+        return foundUser;
     }
 }
