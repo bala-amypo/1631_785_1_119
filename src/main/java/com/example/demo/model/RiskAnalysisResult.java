4889a75e4115259b1d710db164ffdaaad048cd1c
bala-amypo <bala@amypo.in>
2025-12-21 20:21:38 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskAnalysisResult.java
+++ src/main/java/com/example/demo/model/RiskAnalysisResult.java
@@ -1,41 +1,38 @@
-// package com.example.demo.model;
-// import jakarta.persistence.Entity;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
-// import lombok.AllArgsConstructor;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.validation.constraints.NotNull;
-// // import jakarta.persistence.JoinColumn;
-// import java.time.LocalDateTime;
-// // import jakarta.persistence.ManyToOne;
-// import jakarta.persistence.PrePersist;
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
 
-// @Entity
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
+import java.time.LocalDateTime;
+import jakarta.persistence.PrePersist;
 
-// public class RiskAnalysisResult{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     @NotNull
-//     // @ManyToOne
-//     // @JoinColumn(name = "portfolio_id")
-//     private Long portfolioId;
-//     private LocalDateTime analysis;
-//     private Double highestStockPercentage;
-//     private Double highestSectorPercentage;
-//     private boolean isHighRisk;
-//     private String notes;
-//     @PrePersist
-//     public void Onrecord(){
-//         if(this.analysis == null){
-//             LocalDateTime resulted= LocalDateTime.now();
-//             this.analysis=resulted;
-//         }
-//     }
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
-// }
+public class RiskAnalysisResult{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private Long portfolioId;
+    private LocalDateTime analysis;
+    private Double highestStockPercentage;
+    private Double highestSectorPercentage;
+    private boolean isHighRisk;
+    private String notes;
+    @PrePersist
+    public void Onrecord(){
+        if(this.analysis == null){
+            LocalDateTime resulted= LocalDateTime.now();
+            this.analysis=resulted;
+        }
+    }
+
+}
