b8d8482dfdd96aae36a7eba0bb86e77ea81fa8f4
bala-amypo <bala@amypo.in>
2025-12-21 06:18:47 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
@@ -2,3 +2,44 @@
 // public class RiskAnalysisResultServiceImpl implements RiskAnalysisResultService {
     
 // }
+package com.example.demo.service.Impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.example.demo.model.RiskThreshold;
+import com.example.demo.service.RiskThresholdService;
+import com.example.demo.repository.RiskThresholdRepository;
+import com.example.demo.exception.ResourceNotFoundException;
+
+@Service
+public class RiskThresholdServiceImpl implements RiskThresholdService{
+    @Autowired RiskThresholdRepository thresholds;
+    @Override
+    public RiskThreshold createThreshold(RiskThreshold threshold){
+        return thresholds.save(threshold);
+    }
+    @Override
+   public RiskThreshold updateThreshold(Long id,RiskThreshold threshold){
+        if(thresholds.existsById(id)){
+            threshold.setId(id);
+            return thresholds.save(threshold);
+        }
+        return null;
+   }
+    @Override
+    public List<RiskThreshold> getActiveThreshold(){
+        return thresholds.findByActiveTrue();
+    }
+   @Override
+   public RiskThreshold getThresholdById(Long id){
+        return thresholds.findById(id).orElseThrow(()->new ResourceNotFoundException("Portfolio Not found"));
+
+   }
+    @Override
+   public List<RiskThreshold>getAllThresholds(){
+        return thresholds.findAll();
+
+   }
+
+
+}
--- src/main/java/com/example/demo/service/RiskAnalysisResultService.java
+++ src/main/java/com/example/demo/service/RiskAnalysisResultService.java
@@ -5,5 +5,5 @@ import com.example.demo.model.RiskAnalysisResult;
 public interface RiskAnalysisResultService{
     RiskAnalysisResult analyzePortfolio(Long portfolioId);
     RiskAnalysisResult getAnalysisById(Long id);
-    RiskAnalysisResult getAnalysesForPortfolio(Long portfolioId);
+    List<RiskAnalysisResult> getAnalysesForPortfolio(Long portfolioId);
 }
