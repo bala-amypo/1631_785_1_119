3b21321fd515f846be669acf0495062e1c669ffd
bala-amypo <bala@amypo.in>
2025-12-21 06:24:36 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
@@ -1,7 +1,4 @@
-// package com.example.demo.service.Impl;
-// public class RiskAnalysisResultServiceImpl implements RiskAnalysisResultService {
-    
-// }
+
 package com.example.demo.service.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,29 +14,17 @@ public class RiskAnalysisResultServiceImpl implements RiskAnalysisResultService{
     @Override
     public RiskAnalysisResult analyzePortfolio(RiskAnalysisResult Result){
         return Results.save(Result);
-//     }
-//     @Override
-//    public RiskThreshold updateThreshold(Long id,RiskThreshold threshold){
-//         if(thresholds.existsById(id)){
-//             threshold.setId(id);
-//             return thresholds.save(threshold);
-//         }
-//         return null;
-//    }
-//     @Override
-//     public List<RiskThreshold> getActiveThreshold(){
-//         return thresholds.findByActiveTrue();
-//     }
-//    @Override
-//    public RiskThreshold getThresholdById(Long id){
-//         return thresholds.findById(id).orElseThrow(()->new ResourceNotFoundException("Portfolio Not found"));
+    }
+       
+   @Override
+   public RiskAnalysisResult getAnalysisById(Long id){
+        return Results.findById(id).orElseThrow(()->new ResourceNotFoundException("Portfolio Not found"));
 
-//    }
+   }
 //     @Override
 //    public List<RiskThreshold>getAllThresholds(){
 //         return thresholds.findAll();
 
 //    }
-
 
 }
