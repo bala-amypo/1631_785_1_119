c19b37f83bfeffdf85aa2269c7ee2cca26f93709
bala-amypo <bala@amypo.in>
2025-12-21 20:24:38 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RiskAnalysisResultServiceImpl.java
@@ -1,46 +1,36 @@
+package com.example.demo.service.Impl;
 
-// package com.example.demo.service.Impl;
-// // import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import java.util.List;
-// import com.example.demo.model.RiskAnalysisResult;
-// import com.example.demo.service.RiskAnalysisResultService;
-// import com.example.demo.repository.RiskAnalysisResultRepository;
-// import com.example.demo.repository.UserPortfolioRepository;
-// import com.example.demo.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.example.demo.model.PortfolioHolding;
+import com.example.demo.service.PortfolioHoldingService;
+import com.example.demo.repository.PortfolioHoldingRepository;
+import com.example.demo.exception.ResourceNotFoundException;
 
-// @Service
-// public class RiskAnalysisResultServiceImpl implements RiskAnalysisResultService{
-//      private final RiskAnalysisResultRepository Results;
-//      private final UserPortfolioRepository Upr;
-
-//     public RiskAnalysisResultServiceImpl(RiskAnalysisResultRepository Results,UserPortfolioRepository Upr){
-//             this.Results=Results;
-//             this.Upr=Upr;
-//     }
+@Service
+public class PortfolioHoldingServiceImpl implements PortfolioHoldingService{
+    private final PortfolioHoldingRepository holdingRepository;
     
-//     @Override
-//     public RiskAnalysisResult analyzePortfolio(Long portfolioId){
-//           Upr.findById(portfolioId).orElseThrow(()->new ResourceNotFoundException("Portfolio Not found"));
-//           RiskAnalysisResult result=new RiskAnalysisResult();
-//           result.setPortfolioId(portfolioId);
-//           result.setHighestStockPercentage(analysisRequest.getHighestStockPercentage());
-//           result.setHighestSectorPercentage(analysisRequest.getHighestSectorPercentage());
-//           result.setIsHighRisk(analysisRequest.getIsHighRisk());
-//           result.setNotes(analysisRequest.getNotes());
-//           return Results.save(result);
-//      }
+    public PortfolioHoldingServiceImpl(PortfolioHoldingRepository holdingRepository) {
+        this.holdingRepository = holdingRepository;
+    }
+    // @Override
+    // public PortfolioHolding createHolding(PortfolioHolding Holding){
+    //     return holdingRepository.save(Holding);
+    // }
+    
+    
+   @Override
+   public PortfolioHolding getHoldingById(Long id){
+        return holdingRepository.findById(id).orElseThrow(()->new ResourceNotFoundException(" Portfolio Not found"));
 
-       
-//    @Override
-//    public RiskAnalysisResult getAnalysisById(Long id){
-//         return Results.findById(id).orElseThrow(()->new ResourceNotFoundException("Portfolio Not found"));
+   }
+    @Override
+   public List<PortfolioHolding>getHoldingsByPortfolio(){
+        return holdingRepository.findAll();
 
-//    }
-//     @Override
-//    public List<RiskAnalysisResult>getAnalysesForPortfolio(Long portfolioId){
-//         return Results.findByPortfolioId(portfolioId);
+   }
+   
 
-//    }
 
-// }
+}
