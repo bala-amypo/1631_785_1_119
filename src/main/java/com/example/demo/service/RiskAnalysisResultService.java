5aca198b727919e31802a55b48e0ee9c477f207f
bala-amypo <bala@amypo.in>
2025-12-21 06:12:48 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskAnalysisResultService.java
+++ src/main/java/com/example/demo/service/RiskAnalysisResultService.java
@@ -1,4 +1,8 @@
-// package com.example.demo.service;
+
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.model.RiskAnalysisResult;
+
 // public interface RiskAnalysisResultService{
     
-// }
+}
