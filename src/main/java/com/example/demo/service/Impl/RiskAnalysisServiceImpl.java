4ba4beb2b053fe9f50c8c98d4d9d2ed1d96e6778
bala-amypo <bala@amypo.in>
2025-12-22 10:23:27 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskAnalysisServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RiskAnalysisServiceImpl.java
@@ -1,3 +1,10 @@
+package com.example.demo.service.Impl; 
+import org.springframework.stereotype.Service; 
+import java.util.List; 
+import com.example.demo.model.RiskAnalysisResult; 
+import com.example.demo.service.RiskAnalysisService; 
+import com.example.demo.repository.RiskAnalysisResultRepository; 
+import com.example.demo.exception.ResourceNotFoundException;
 @Service
 public class RiskAnalysisServiceImpl implements RiskAnalysisService {
 
