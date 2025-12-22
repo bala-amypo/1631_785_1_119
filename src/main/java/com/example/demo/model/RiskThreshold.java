95afa64b15a6a19b502e1350c4d1f6ded14d71e3
bala-amypo <bala@amypo.in>
2025-12-22 10:40:47 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskThreshold.java
+++ src/main/java/com/example/demo/model/RiskThreshold.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.persistence.NotNull;
+
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.validation.constraints.NotNull;
