d7bf425ecef1e269964a9c736aeda841ff7a5509
bala-amypo <bala@amypo.in>
2025-12-22 10:29:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,38 +1,34 @@
 package com.example.demo.controller;
 
-
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    
+
     private final UserService userService;
-    
+
     public AuthController(UserService userService) {
         this.userService = userService;
     }
-    
+
+    // Register a new user
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
-        return userService.registerUser(user);
+        return userService.register(user);
     }
-    
+
     @PostMapping("/login")
     public User loginUser(@RequestBody User user) {
         User foundUser = userService.findByEmail(user.getEmail());
-        
-        if(!foundUser.getPassword().equals(user.getPassword())) {
+
+        if (foundUser == null || !foundUser.getPassword().equals(user.getPassword())) {
             throw new IllegalArgumentException("Invalid credentials");
         }
-        
+
         return foundUser;
     }
 }
