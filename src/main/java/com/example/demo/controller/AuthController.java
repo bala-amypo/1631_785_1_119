0182e255ceae00032b1e359b9131152f3db04e38
bala-amypo <bala@amypo.in>
2025-12-26 14:26:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -27,6 +27,6 @@ public class AuthController {
             throw new IllegalArgumentException("Invalid credentials");
         }
 
-        return ResponseEntity.ok(foundUser);
+        return ResponseEntity.ok("Login successful");
     }
 }
