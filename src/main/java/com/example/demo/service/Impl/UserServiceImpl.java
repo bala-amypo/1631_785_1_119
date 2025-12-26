d9372fc6a996462b91108023d256d153914e0641
bala-amypo <bala@amypo.in>
2025-12-26 04:25:03 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -22,7 +22,7 @@ public class UserServiceImpl implements UserService {
             user.setRole("MONITOR");
         }
         if (userRepository.findByEmail(user.getEmail()) != null) {
-            throw new RuntimeException("Email already registered");
+            throw new ResourceNotFoundException("Email already registered");
         }
         return userRepository.save(user);
     }
